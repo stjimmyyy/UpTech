26e26399af78ddf126e7bdd9259576cf04b31f4b
dimitar <stifki234@abv.bg>
2021-07-25 23:13:53 +0300

    Reinitialized migrations to work with with SQL server and built the database

--- UpTech/Startup.cs
+++ UpTech/Startup.cs
@@ -28,7 +28,7 @@ namespace UpTech
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
